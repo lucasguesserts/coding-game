a589b5f84fb236b46276dfedbdf9437458ef3405
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2021-09-06 10:30:13 -0300

    refactor: improve haskell solution of custom game of life

--- haskell/custom-game-of-life/custom_game_of_life.hs
+++ haskell/custom-game-of-life/custom_game_of_life.hs
@@ -1,144 +1,97 @@
 import System.IO
 import Control.Monad
 
-data Cell = Alive | Dead deriving (Eq, Show)
+indexed :: [a] -> [(Int, a)]
+indexed = go 0
+  where
+    go i (a:as) = (i, a) : go (i + 1) as
+    go _ _      = []
+
+type Cell = Char
+
+alive :: Char
+alive = 'O'
+dead :: Char
+dead = '.'
 
 -- input
 
-getHWN = map (read :: String -> Int) . words <$> getLine
-getRawLivenessCondition = getLine
+getRawInitialCondition :: Int -> IO [String]
 getRawInitialCondition = flip replicateM getLine
-
-charToCell :: Char -> Cell
-charToCell char = if char == 'O' then Alive else Dead
-
-charsToCells :: [Char] -> [Cell]
-charsToCells = map charToCell
-
-rawInitialConditionToCells :: [[Char]] -> [[Cell]]
-rawInitialConditionToCells = map charsToCells
-
-skipLine :: IO ()
-skipLine = putStrLn ""
-
-displayCells :: [[Cell]] -> IO ()
-displayCells cells = do
-  skipLine
-  putStrLn "Game status:"
-  putStrLn . unlines . map (show . show) $ cells
-  skipLine
 
 -- output
 
-cellToChar :: Cell -> Char
-cellToChar cell = if cell == Alive then 'O' else '.'
-
-cellsToChars :: [Cell] -> [Char]
-cellsToChars = map cellToChar
-
-cellsToOutput :: [[Cell]] -> [[Char]]
-cellsToOutput = map cellsToChars
-
 printOutput :: [[Cell]] -> IO ()
-printOutput cells = putStr . unlines $ cellsToOutput cells
-
-displayOutput :: [[Cell]] -> IO ()
-displayOutput cells = do
-  skipLine
-  putStrLn "Game status:"
-  printOutput cells
-  skipLine
+printOutput = putStr . unlines
 
 -- surrounding cells
 
-makeDeadLine :: [[Cell]] -> [Cell]
-makeDeadLine cells = replicate (length (head cells)) Dead
-
-addDeadCells :: [[Cell]] -> [[Cell]]
-addDeadCells = map (\cells -> Dead : cells ++ [Dead])
-
-addDeadCellsOnTopAndBottom :: [[Cell]] -> [[Cell]]
-addDeadCellsOnTopAndBottom init = makeDeadLine init : init ++ [makeDeadLine init]
-
-addAllDeadCells :: [[Cell]] -> [[Cell]]
-addAllDeadCells = addDeadCellsOnTopAndBottom . addDeadCells
-
 getSurroundingCells :: (Int, Int) -> [[Cell]] -> [Cell]
-getSurroundingCells (x,y) cells = [
-    cells !! (y-1) !! (x-1)
-  , cells !! (y-1) !!  x
-  , cells !! (y-1) !! (x+1)
-  , cells !!  y    !! (x-1)
-  , cells !!  y    !! (x+1)
-  , cells !! (y+1) !! (x-1)
-  , cells !! (y+1) !!  x
-  , cells !! (y+1) !! (x+1)
-  ]
+getSurroundingCells (cx,cy) cells = do
+  [cells !! y !! x | x <- [xo..xf], y <- [yo..yf], (x, y) /= (cx, cy)]
+  where
+      limitY = length cells - 1
+      limitX = (length . head) cells - 1
+      xo = max 0 (cx - 1)
+      xf = min limitX (cx + 1)
+      yo = max 0 (cy - 1)
+      yf = min limitY (cy + 1)
 
 getMainCell :: (Int, Int) -> [[Cell]] -> Cell
 getMainCell (x,y) cells = cells !! y !! x
 
 -- make coordinates
 
-makeMyList :: Int -> Int -> Int -> [(Int, Int)]
-makeMyList y i limit
-  |  i == limit = []
-  |  otherwise = (i,y) : makeMyList y (i+1) limit
-
-makeMyLargerList :: Int -> Int -> Int -> [[(Int, Int)]]
-makeMyLargerList i h w
-  | i == h+1 = []
-  | otherwise = makeMyList i 1 (w+1) : makeMyLargerList (i+1) h w
-
 makeCoordinates :: Int -> Int -> [[(Int, Int)]]
-makeCoordinates = makeMyLargerList 1
+makeCoordinates h w = [[(x,y) | x <- [0..xf]] | y <- [0..yf]]
+    where
+        xf = max 0 (w - 1)
+        yf = max 0 (h - 1)
 
 -- liveness function
 
-rawLivenessConditionToList :: Int -> [Char] -> [Int] -> [Int]
-rawLivenessConditionToList _ [] l  = l
-rawLivenessConditionToList n (c:cs) l = rawLivenessConditionToList (n+1) cs (if c == '1' then n : l else l)
-
 toLivenessList :: [Char] -> [Int]
-toLivenessList raw = rawLivenessConditionToList 0 raw []
+toLivenessList raw = map fst (filter (\(idx, char) -> char == '1') (indexed raw))
 
 countAlive :: [Cell] -> Int
-countAlive = length . filter (==Alive)
+countAlive = length . filter (==alive)
 
 toLivenessFunction :: [Char] -> ([Cell] -> Cell)
 toLivenessFunction raw cells = if
   or ((==) <$> toLivenessList raw <*> [countAlive cells])
-  then Alive
-  else Dead
+  then alive
+  else dead
 
 -- map to new cells
 
 makeNewCell :: (Int, Int) -> [[Cell]] -> ([Cell] -> Cell) -> ([Cell] -> Cell) -> Cell
 makeNewCell coordinate cells aliveFunction birthFuntion =
-  if mainCell == Alive
+  if mainCell == alive
   then aliveFunction surroundingCells
   else birthFuntion surroundingCells
   where
     surroundingCells = getSurroundingCells coordinate cells
     mainCell = getMainCell coordinate cells
 
-makeNewCellsRow :: [(Int, Int)] -> [[Cell]] -> ([Cell] -> Cell) -> ([Cell] -> Cell) -> [Cell]
-makeNewCellsRow rowCoordinates cells aliveFunction birthFuntion =
-  map (\coord -> makeNewCell coord cells aliveFunction birthFuntion) rowCoordinates
-
 makeNewCells :: [[(Int, Int)]] -> [[Cell]] -> ([Cell] -> Cell) -> ([Cell] -> Cell) -> [[Cell]]
-makeNewCells allCoordinates cells aliveFunction birthFuntion =
-  map (\rowCoordinates -> makeNewCellsRow rowCoordinates cells aliveFunction birthFuntion) allCoordinates
+makeNewCells coordinates cells aliveFunction birthFunction =
+  [
+    [
+      makeNewCell coord cells aliveFunction birthFunction
+      | coord <- row
+      ]
+    | row <- coordinates
+  ]
 
 -- join all
 
 simulate :: Int -> Int -> Int -> [[Cell]] -> ([Cell] -> Cell) -> ([Cell] -> Cell) -> [[Cell]]
-simulate simStep h w cells aliveFunction birthFunction
-  | simStep == 0 = cells
-  | otherwise = simulate (simStep-1) h w newCells aliveFunction birthFunction
+simulate n h w cells aliveFunction birthFunction
+  | n == 0 = cells
+  | otherwise = simulate (n-1) h w newCells aliveFunction birthFunction
   where
     coordinates = makeCoordinates h w
-    newCells = makeNewCells coordinates (addAllDeadCells cells) aliveFunction birthFunction
+    newCells = makeNewCells coordinates cells aliveFunction birthFunction
 
 -- MAIN
 
@@ -146,16 +99,12 @@ main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering -- DO NOT REMOVE
 
-  [h, w, n] <- getHWN
+  [h, w, n] <- map (read :: String -> Int) . words <$> getLine
 
-  aliveRawCondition <- getRawLivenessCondition
-  birthRawCondition <- getRawLivenessCondition
-  initialRawCondition <- getRawInitialCondition h
+  aliveFunction <- toLivenessFunction <$> getLine
+  birthFunction <- toLivenessFunction <$> getLine
+  cells <- replicateM h getLine
 
-  let aliveTransformationFunction = toLivenessFunction aliveRawCondition
-  let birthTransformationFunction = toLivenessFunction birthRawCondition
-  let cells = rawInitialConditionToCells initialRawCondition
-
-  printOutput $ simulate n h w cells aliveTransformationFunction birthTransformationFunction
+  printOutput $ simulate n h w cells aliveFunction birthFunction
 
   return ()
